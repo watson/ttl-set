fcb5712a114e0c63c45c673f33bbc24614cef697
Thomas Watson <w@tson.dk>
2024-11-05 20:45:26 +0000

    Improve tests

--- test.js
+++ test.js
@@ -35,16 +35,28 @@ test('replicate standard Set API', async (t) => {
 })
 
 test('evict items after TTL', async (t) => {
-  const set = new TTLSet(200)
+  const set = new TTLSet(500)
   set.add('foo')
   assert.equal(set.size, 1)
-  await scheduler.wait(100)
+  await scheduler.wait(250)
   set.add('bar')
   assert.equal(set.size, 2)
-  await scheduler.wait(150)
+  await scheduler.wait(400)
   assert.equal(set.size, 1)
   assert.equal(set.has('foo'), false)
   assert.equal(set.has('bar'), true)
-  await scheduler.wait(100)
+  await scheduler.wait(600)
   assert.equal(set.size, 0)
+})
+
+test('unref timer', (t) => {
+  const set = new TTLSet(0)
+  set.add('foo')
+  for (const sym of Object.getOwnPropertySymbols(set._timer)) {
+    if (sym.description === 'refed') {
+      assert.equal(set._timer[sym], false)
+      return
+    }
+  }
+  assert.fail('Symbol(refed) not found')
 })
